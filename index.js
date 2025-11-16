3470c1f7776550d19a1137df4a4f4fc4bcfac87e
Gowtham Yarlagadda <gowtham777y@gmail.com>
2025-11-16 18:08:14 +0530

    Getting the data from the API through GET request

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const colorForm = document.getElementById("selector-form")
+const baseURL = "https://www.thecolorapi.com/scheme"
 
 colorForm.addEventListener('submit',function(e){
     e.preventDefault()
     const formData = new FormData(colorForm)
-    console.log(formData.get("color-picker"))
+    const hexCode = formData.get("color-picker").slice(1)
+    const selectedOption = formData.get("selectedOption")
+    fetch(baseURL+`?hex=${hexCode}&mode=${selectedOption}`)
+    .then(response=>{
+        if (!response.ok){
+            console.log(`HTTP Error! status ${response.status}`)
+        }
+        return response.json()
+    }).then(data=>{
+        const colorsArray = data.colors.map(color => {
+            return color.hex.value
+        })
+        fillColors(colorsArray)
+    }).catch(error=>{
+        console.log("Error Fetching the data: ", error)
+    })
 })
